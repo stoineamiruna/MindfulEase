b49047a6ac2c0a3f022dd688b61b1fa410eac33d
Stoinea Maria-Miruna <130232381+stoineamiruna@users.noreply.github.com>
2025-03-15 22:11:00 +0200

    T20 Statistici si Progres

---
+++ MindfulEase/Data/Migrations/20250313125310_Update2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MindfulEase.Data.Migrations
+{
+    public partial class Update2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ApplicationUserEmotions",
+                table: "ApplicationUserEmotions");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ApplicationUserEmotions",
+                table: "ApplicationUserEmotions",
+                columns: new[] { "UserId", "EmotionId", "Date" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ApplicationUserEmotions",
+                table: "ApplicationUserEmotions");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ApplicationUserEmotions",
+                table: "ApplicationUserEmotions",
+                columns: new[] { "UserId", "EmotionId" });
+        }
+    }
+}
