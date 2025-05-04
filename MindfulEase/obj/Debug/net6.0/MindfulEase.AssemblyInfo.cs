7d0f8e07e04536ca9c9eb6b0f499ebb949c86515
Stoinea Maria-Miruna <130232381+stoineamiruna@users.noreply.github.com>
2025-05-05 01:23:28 +0300

    T33 Adaugare tab Brain Projection + brain controls

--- MindfulEase/obj/Debug/net6.0/MindfulEase.AssemblyInfo.cs
+++ MindfulEase/obj/Debug/net6.0/MindfulEase.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MindfulEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b86de226903b0af11a4d6b9de905da7102f00cfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d878449d0c74df8c656708a775a53dba262a90a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MindfulEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MindfulEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
